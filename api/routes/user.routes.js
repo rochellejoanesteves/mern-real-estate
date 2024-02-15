fb9c700f5857e3b7ed032164017ce2e3de284da1
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-02-15 13:14:37 +0800

    checking prod bug

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listings/:id", getUserListing);
+router.get("/listings/:id", verifyToken, getUserListing);
 router.get("/:id", verifyToken, getUser);
 
 export default router;
